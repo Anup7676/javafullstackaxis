a3524e268550e6b2c755da3220f67d5d29f99cd1
Anup7676 <anupallie@gmail.com>
2023-04-14 15:27:00 +0530

    refractoring

---
+++ VS code Axis/40 day/spring-data-demo/src/main/java/com/ani/data/config/RepositoryConfiguration.java
@@ -0,0 +1,10 @@
+package com.ani.data.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// as we use spring boot, we dont need this, spring boot data starter does this automaticaly
+// @EnableJpaRepositories("com.ani.data.repository")
+@Configuration
+public class RepositoryConfiguration {
+    
+}
