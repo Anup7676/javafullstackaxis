ab63786ba4c2822585f2e5d844a238b45ddca47f
Anup7676 <anupallie@gmail.com>
2023-04-17 17:10:40 +0530

    safe commit

--- VS code Axis/42 day/hibernate-demo/src/main/java/com/ani/orm/domain/Car.java
+++ VS code Axis/42 day/hibernate-demo/src/main/java/com/ani/orm/domain/Car.java
@@ -1,12 +1,16 @@
 package com.ani.orm.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Setter
 @Getter
 @Entity
@@ -14,6 +18,7 @@ import lombok.Setter;
 public class Car {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private Double cost;
